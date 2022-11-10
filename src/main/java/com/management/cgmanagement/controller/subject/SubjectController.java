bb9c589fca044c14475b5a02d895ce6fb50325ab
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-11-10 16:53:10 +0700

    update

--- src/main/java/com/management/cgmanagement/controller/subject/SubjectController.java
+++ src/main/java/com/management/cgmanagement/controller/subject/SubjectController.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 public class SubjectController {
     @Autowired
     private SubjectService subjectService;
-    @GetMapping("/list")
+    @GetMapping()
     public ResponseEntity<Iterable<Subject>> findAllSubject(){
         List<Subject> subject = (List<Subject>)subjectService.findAll();
         if (subject.isEmpty()){
@@ -33,12 +33,12 @@ public class SubjectController {
         return new ResponseEntity<>(subjects.get(),HttpStatus.OK);
     }
 
-    @PostMapping("/create")
+    @PostMapping()
     public ResponseEntity<Subject> save(@RequestBody Subject subject){
         return new ResponseEntity<>(subjectService.save(subject), HttpStatus.CREATED);
     }
 
-    @PutMapping("/update/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Subject> updateSubject(@PathVariable Long id,@RequestBody Subject subject){
         Optional<Subject> subjectOptional = subjectService.findById(id);
         if (!subjectOptional.isPresent()) {
@@ -48,7 +48,7 @@ public class SubjectController {
         return new ResponseEntity<>(subjectService.save(subject),HttpStatus.OK);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Subject> deleteSubject(@PathVariable Long id){
         Optional<Subject> subjectOptional = subjectService.findById(id);
         if (!subjectOptional.isPresent()){
