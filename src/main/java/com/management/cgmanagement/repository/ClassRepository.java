e91d98ce45202db6610009424f9bf8c4c994cd0b
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-11-11 15:58:04 +0700

    classroomrepository

--- src/main/java/com/management/cgmanagement/repository/ClassRepository.java
+++
@@ -1,7 +0,0 @@
-package com.management.cgmanagement.repository;
-
-import com.management.cgmanagement.model.entity.ClassRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClassRepository extends JpaRepository<ClassRoom, Long> {
-}
