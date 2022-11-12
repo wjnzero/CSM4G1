d8d33f1cee61281c07b27b2ab517bbcec17623db
NguyenThuong-code <you@example.com>
2022-11-12 11:21:43 +0700

    AJAX for add Story

--- FE/js/Classroom.js
+++ FE/js/Classroom.js
@@ -17,29 +17,10 @@ function  successDisplayClass(){
         }
     });
 }
-// function  successDisplayClass(){
-//     $.ajax({
-//         type: "GET",
-//         url: "http://localhost:8080/classes",
-//         success: function (data) {
-//             console.log("data ",data)
-//             let content= '  <tr>\n'+
-//                 '       <td>nameClass</td>\n'+
-//                 '        <td>numberStudent</td>\n'+
-//                 '        <td>AddStory</td>\n'+
-//                 '</tr>';
-//             for (let i = 0; i<data.length; i++){
-//                 content += getClassRoom(data[i]);
-//             }
-//             document.getElementById("displayClassroom").innerHTML= content;
-//         },
-//         error: function (err) {
-//             console.log(err)
-//         }
-//     })
-// }
+function addStory(){
+    window.location.href="../story/formstory.html"
+}
 function  getClassRoom(classroom){
     return `<tr><td >${classroom.nameClass}</td><td >${classroom.numberStudent}</td>`+
-       `<td><a class="addStory" onclick='addNewStory($(this))'>Add Story</a></td>
- </tr>`
+       `<td><button  type="submit" onclick='addStory()'>Add Story</button></td></tr>`
 }
