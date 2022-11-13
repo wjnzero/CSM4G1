4b857e56b2ef9528a49a46a2950ae5ad4f8a7154
wjnzero <chuckieakenzzz@gmai.com>
2022-11-14 01:50:46 +0700

    show user

--- src/main/java/com/management/cgmanagement/controller/admin/AdminController.java
+++ src/main/java/com/management/cgmanagement/controller/admin/AdminController.java
@@ -1,17 +1,21 @@
 package com.management.cgmanagement.controller.admin;
 
 
+import com.management.cgmanagement.model.dto.ITuition;
+import com.management.cgmanagement.model.entity.ClassRoom;
 import com.management.cgmanagement.model.entity.Course;
 import com.management.cgmanagement.model.entity.Role;
 import com.management.cgmanagement.model.entity.User;
 import com.management.cgmanagement.service.course.ICourseService;
 import com.management.cgmanagement.service.jwt.TokenService;
+import com.management.cgmanagement.service.user.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -23,6 +27,9 @@ public class AdminController {
     @Autowired
     ICourseService courseService;
 
+    @Autowired
+    IUserService userService;
+
     @PostMapping("/create-course")
     public ResponseEntity<Course> register(@RequestBody Course course){
         try {
@@ -33,6 +40,20 @@ public class AdminController {
             e.printStackTrace();
         }
         return null;
+    }
+    @GetMapping("/all-users")
+    public ResponseEntity<Iterable<User>> getAllUser(){
+        Iterable<User> users = null;
+        try {
+            users = userService.getUsersNative();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+//        if (courses.isEmpty()) {
+//            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//        }
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/management/cgmanagement/service/user/IUserService.java
+++ src/main/java/com/management/cgmanagement/service/user/IUserService.java
@@ -1,5 +1,6 @@
 package com.management.cgmanagement.service.user;
 
+import com.management.cgmanagement.model.dto.ITuition;
 import com.management.cgmanagement.model.entity.User;
 import com.management.cgmanagement.service.GenericService;
 
@@ -8,4 +9,5 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface IUserService extends GenericService<User>, UserDetailsService {
     User findByEmail(String email);
+    Iterable<User> getUsersNative();
 }
