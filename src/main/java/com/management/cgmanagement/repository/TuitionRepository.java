3490d606ff3e877c6c91ee231d3fe02100b86bf7
HanPham <petphamhandc@gmail.com>
2022-11-13 13:44:21 +0700

    add

--- src/main/java/com/management/cgmanagement/repository/TuitionRepository.java
+++ src/main/java/com/management/cgmanagement/repository/TuitionRepository.java
@@ -10,6 +10,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TuitionRepository extends JpaRepository<Tuition, Long> {
-    @Query(nativeQuery = true ,value = "select `users`.`full_name`,`classes`.`name_class`,`course`.`name`,`tuition`.`completed_fee`, `tuition`.`total_fee` from users join user_class on `users`.`id` = `user_class`.`user_id` join classes on `user_class`.`class_room_id` = `classes`.`id` join `course` on `users`.`id` = `course`.`user_id` join `tuition` on `course`.`id` = `tuition`.`course_id`;")
+    @Query(nativeQuery = true ,value = "select `users`.`full_name`,`classes`.`name_class`,`course`.`name`,`tuition`.`completed_fee`, `tuition`.`total_fee`,`tuition`.`debt` from users join user_class on `users`.`id` = `user_class`.`user_id` join classes on `user_class`.`class_room_id` = `classes`.`id` join `course` on `users`.`id` = `course`.`user_id` join `tuition` on `course`.`id` = `tuition`.`course_id`;")
     Iterable<ITuition> getTuitionNative();
 }
